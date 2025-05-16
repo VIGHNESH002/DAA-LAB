f0ad676a1cfeb81aaf7428b4c0f18b8cb9a4ea76
BVIGHNESH <159248030+VIGHNESH002@users.noreply.github.com>
2025-05-16 10:21:54 +0530

    Update problem10(travelling salesman).c

--- problem10(travelling salesman).c
+++ problem10(travelling salesman).c
@@ -28,3 +28,16 @@ int main(){
     printf("Minimum cost using greedy approach: %d\n",result);
     return 0;
 }
+OUTPUT:-
+Enter the number of cities: 3
+Enter the distance matrix:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+Minimum cost using greedy approach: 15
